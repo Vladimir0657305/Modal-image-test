7c0db2f00f51d0ee28e7486d54341d8c1196636e
Vladimir0657305 <vv0657305@gmail.com>
2022-08-10 13:52:33 +0300

    changes

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //Get the modal
-let modal = document.getElementById('divModal');
+let modal = document.querySelector('.divModal');
 
 //Get the image
-let img = document.getElementById('photo');
-let modalImg = document.getElementById('img01');
+let img = document.querySelector('.image');
+let modalImg = document.querySelector('.image_modal');
 let captionText = document.getElementById("botomtext");
 
+modal.classList.remove('divModal');
 img.onclick = function () {
+    modal.classList.add('divModal');
     modal.style.display = "block";
     modalImg.src = this.src;
     modalImg.alt = this.alt;
@@ -18,4 +20,7 @@ let span = document.getElementsByClassName("close__button")[0];
 //click on (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
+    // modal.classList.add('closeClass');
+    // modal.classList.remove('divModal');
+    // document.querySelector('.divModal').style.display = 'none';
 }
